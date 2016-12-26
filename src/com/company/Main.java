ab9e5d5fd8b9dc88b2b43459a6022b14281cf349
Roman <bioelectromecha@gmail.com>
2016-12-26 17:51:10 +0200

    solved the maman

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,7 +5,6 @@ public class Main {
     public static void main(String[] args) {
         int[] arr1 = RandomGenerator.generateRandomizedArray(10);
         HeapHandler heapHandler = new HeapHandler(3);
-
         heapHandler.buildMaxHeap(arr1);
         printArray(arr1);
 
