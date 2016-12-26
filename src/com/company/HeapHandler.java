e04b5dc630f07eb6b551003dc4fbc9aaade865ba
amit nissan <amitnissan20@gmail.com>
2016-12-26 15:43:09 +0200

    Added MAX_HEAPIFY function

--- src/com/company/HeapHandler.java
+++ src/com/company/HeapHandler.java
@@ -19,12 +19,6 @@ public class HeapHandler {
         return arr[i * mNumOfChildren + son];
     }
 
-    public int[] buildMaxHeap(int[] arr) {
-        for (int i = arr.length/2; i >= 0; i--) {
-        }
-        return new int[0];
-    }
-
     public int getRightSon(int [] arr, int i)
     {
         return arr[(mNumOfChildren * i) + 1];
@@ -34,6 +28,14 @@ public class HeapHandler {
     {
         return arr[(mNumOfChildren * i) - (mNumOfChildren - 2)];
     }
+
+    private void swap(int a, int b)
+    {
+        int temp = a;
+        a = b;
+        b = temp;
+    }
+
 
     private int maxSon(int[] arr,int i) {
         int maxPos = 0;
@@ -46,4 +48,20 @@ public class HeapHandler {
     }
 
 
+
+    public void MAX_HEAPIFY(int [] arr, int i)
+    {
+        int largest = 0;
+        int l = getLeftSon(arr,i);
+        int r = getRightSon(arr,i);
+        if (l <= arr.length && arr[l] > arr[i])
+            largest = l;
+        else
+            largest = i;
+        if (r <= arr.length && arr[r] > arr[largest])
+            largest = r;
+        if (largest > i)
+            swap(arr[i],arr[largest]);
+        MAX_HEAPIFY(arr,largest);
+    }
 }
