d70983b5301e2b399d641f075d42d33de070c4b6
Roman <bioelectromecha@gmail.com>
2016-12-26 15:38:24 +0200

    added the max son function

--- src/com/company/HeapHandler.java
+++ src/com/company/HeapHandler.java
@@ -19,6 +19,12 @@ public class HeapHandler {
         return arr[i * mNumOfChildren + son];
     }
 
+    public int[] buildMaxHeap(int[] arr) {
+        for (int i = arr.length/2; i >= 0; i--) {
+        }
+        return new int[0];
+    }
+
     public int getRightSon(int [] arr, int i)
     {
         return arr[(mNumOfChildren * i) + 1];
@@ -27,6 +33,16 @@ public class HeapHandler {
     public int getLeftSon(int [] arr, int i)
     {
         return arr[(mNumOfChildren * i) - (mNumOfChildren - 2)];
+    }
+
+    private int maxSon(int[] arr,int i) {
+        int maxPos = 0;
+        for (int j = 1; j < mNumOfChildren; j++) {
+            if (arr[j] > arr[maxPos]) {
+                maxPos = j;
+            }
+        }
+        return maxPos;
     }
 
 
